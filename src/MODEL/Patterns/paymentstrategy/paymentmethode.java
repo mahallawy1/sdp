44773478a740dc1d9ee58fd9b4375a3306957090
Eslam <Eslam@192.168.48.1>
2024-11-08 20:07:10 +0200

    login strategy

---
+++ src/MODEL/Patterns/paymentstrategy/paymentmethode.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODEL.Patterns.paymentstrategy;
+
+/**
+ *
+ * @author belal
+ */
+public class paymentmethode {
+    public IPayments ref;
+    private int id;
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void choosepayment(IPayments X){
+        ref=X;
+    }
+    public void pay(){
+        ref.paymentmethode("payment");
+    }
+   
+   // public static void main(String[] args) {
+        
+      //  paymentmethode pay = new paymentmethode();
+      //  pay.choosepayment(new FawryPayment());
+      //  pay.pay();
+    
+   // }
+        
+    
+}
